81d876cbef8e4e2a70161a90b7d060feaae6d590
jordanavq <jordanavq@gmail.com>
2021-09-14 21:54:09 -0300

    route for post method created

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const Todo = require("./models/Todo");
 //para criar o servidor
 
 const PORT = 5000;
@@ -8,5 +8,24 @@ const PORT = 5000;
 require("./config/db.config");
 
 const app = express();
+
+//utilizar o json no body das requisições
+app.use(express.json());
+
+app.post("/todos", async (request, response) => {
+  if (!req.body.title) {
+    return res.status(400).json({ msg: "missing title field" });
+  }
+  try {
+    //criar novo todo no banco de dados
+    const newTodo = await Todo.create(req.body);
+    //retornar json do todo criado
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(500).json({ msg: `Sever error ${error}` });
+  }
+});
+
+app.get('/todos', (req, res) => )
 
 app.listen(PORT, () => console.log(`Server listen on Port ${PORT}`));
